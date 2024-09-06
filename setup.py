6a59d16d7a3f8a3591e06b8ccfe90b8b348dc4f3
Sumit Singh <sumitsingh4553@gmail.com>
2024-09-06 16:07:01 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'cellSegmentation',
+    version= '0.0.0',
+    author= 'SUMIT SINGH',
+    author_email= 'sumitsingh4553@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
